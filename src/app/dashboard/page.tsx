4668cb24483fa76a49c656ef507578a83da94fa3
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-11-18 18:00:59 +0530

    changes
    changes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import MainStructure from '@/components/custom/Dashboard/MainStructure'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+
+        <MainStructure/>
+
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/components/custom/Dashboard/MainStructure.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainStructure = () => {
+  return (
+    <div>MainStructure</div>
+  )
+}
+
+export default MainStructure
