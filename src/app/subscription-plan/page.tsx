cd26257068511625d5d98dab609900bc83a0ee2a
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-11-20 01:46:37 +0530

    changes
    changes

--- src/app/subscription-plan/page.tsx
+++ src/app/subscription-plan/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import InnerHeroOne from '@/components/custom/Global/InnerHeroOne'
+import DetailCard from './DetailCard'
+import SubscriptionPlanForm from './SubscriptionPlanForm'
 
 const SubscriptionPlan = () => {
   return (
@@ -14,6 +16,56 @@ const SubscriptionPlan = () => {
         subtitle="Get Your upto 3X of monthly income"
         ctaText="Get A Subscription Plan"
         />
+
+
+        
+
+
+
+        <div className="flex flex-col justify-center xl:flex-row">
+
+<div className="flex flex-1 mx-auto px-[20px]  justify-center w-full py-8 ">
+ 
+ <DetailCard/>
+
+</div>
+
+
+
+
+
+<div className="flex  flex-1 mx-auto py-4 px-[20px] max-w-[620px] mt-8">
+
+<SubscriptionPlanForm/>
+
+</div>
+
+
+
+
+
+<div className="flex flex-1 max-w-[620px] w-full  2xl:block">
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
