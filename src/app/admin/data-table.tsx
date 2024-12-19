1a64642cfc577e3aedd8524c80c111ce717d1dea
ayan471 <pakhiraayan@gmail.com>
2024-12-19 14:23:32 +0530

    fixed bug

--- src/app/admin/data-table.tsx
+++ src/app/admin/data-table.tsx
@@ -59,11 +59,9 @@ export function DataTable<TData, TValue>({
       <div className="flex items-center py-4">
         <Input
           placeholder="Filter by name..."
-          value={
-            (table.getColumn("fullName")?.getFilterValue() as string) ?? ""
-          }
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("fullName")?.setFilterValue(event.target.value)
+            table.getColumn("name")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
